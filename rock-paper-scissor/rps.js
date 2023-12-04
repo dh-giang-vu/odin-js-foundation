4aba13b7e439e01ee2cb9ad48beb058f155e70b9
dhgiang <dhgiang.vu@gmail.com>
2023-12-04 21:46:02 +0700

    ui done

--- rock-paper-scissor/rps.js
+++ rock-paper-scissor/rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let rand = Math.random()*3;
     if (rand < 1) {
@@ -15,8 +18,10 @@ function playRound(playerSelection, computerSelection) {
     } else if ((playerSelection == "Paper" && computerSelection == "Rock") ||
         (playerSelection == "Scissor" && computerSelection == "Paper") || 
         (playerSelection == "Rock" && computerSelection == "Scissor")) {
-        return "Player Wins";
+            playerScore += 1;
+            return "Player Wins";
     } else {
+        computerScore += 1;
         return "Player Lost";
     }
 }
@@ -29,3 +34,30 @@ function game() {
         console.log(playRound(playerSelection, computerSelection));
     }
 }
+
+function playerChoice(event) {
+    let btn = event.target;
+    let compSel = getComputerChoice();
+    cs.textContent = "Computer's selection: " + compSel;
+    res.textContent = "Result: " + playRound(btn.textContent, compSel);
+
+    if (playerScore === 5 || computerScore === 5) {
+        ps.removeEventListener("click", playerChoice);
+
+        if (playerScore === 5) {
+            ann.textContent = "Winner: Player";
+        }
+        else {
+            ann.textContent = "Winner: Computer";
+        }
+    }
+}
+
+
+const ps = document.querySelector(".ps");
+const cs = document.querySelector(".cs");
+const res = document.querySelector(".result");
+const ann = document.querySelector(".ann");
+
+ps.addEventListener("click", playerChoice);
+

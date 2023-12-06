7215d9553aab98152204240536190906925e1152
dhgiang <dhgiang.vu@gmail.com>
2023-12-06 14:07:27 +0700

    Logic done

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -11,8 +11,67 @@ function isDigit(symbol) {
     return /^[0-9]$/.test(symbol);
 }
 
+
 function performOperation() {
-   return 0;
+    let first = 0;
+    let op = undefined;
+    let second = 0;
+
+    if (isDigit(input[input.length-1]) === false) {
+        input = [];
+        return "ERROR";
+    }
+
+    // Convert input to expression
+    for (let i = 0; i < input.length; i++) {
+        if (isDigit(input[i]) === false) {
+            op = input[i];
+            console.log(op, input[i]);
+            continue;
+        }
+
+        if (op === undefined) {
+            first = first * 10 + Number(input[i]);
+        }
+        else {
+            second = second * 10 + Number(input[i]);
+        }
+    }
+
+    let result = 0;
+
+    // Get result
+    switch(op) {
+        case "+":
+            result = first + second;
+            break;
+        case "-":
+            result = first - second;
+            break;
+        case "*":
+            result = first * second;
+            console.log("m" + result);
+            break;
+        case "/":
+            if (second === 0) {
+                input = [];
+                return "ERROR";
+            }
+            result = Math.round(first / second);
+            break;
+        default:
+            result = first;
+    }
+
+    input = [];
+
+    // Parse into input
+    let temp = result.toString();
+    for (let i = 0; i < temp.length; i++) {
+        input.push(temp[i]);
+    }
+
+    return result;
 }
 
 
@@ -52,10 +111,9 @@ buttons.forEach(function(button) {
         }
     
         if (button.textContent === "=") {
-            // Perform Calculations
-            // Update input
-            // Update display
+            display.textContent = performOperation()
             console.log("Perform operation");
+            hasOp = false;
             return;
         }
     
@@ -64,9 +122,7 @@ buttons.forEach(function(button) {
                 return;
             }
             if (hasOp) {
-                // Perform Calculations
-                // Update input
-                // Update display
+                display.textContent = performOperation();
                 console.log("Perform operation");
             }
             else {
